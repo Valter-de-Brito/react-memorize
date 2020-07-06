bdded37c544d57378b88c3d547e3a7e7a7584df0
Valter-de-Brito <valter.dbrito@gmail.com>
2020-07-06 21:26:48 +0100

    "FaceUp the card"

--- src/Memorize.tsx
+++ src/Memorize.tsx
@@ -1,25 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import "./Memorize.css";
 
-function Memorize() {
+interface ICard {
+  id: number;
+  content: string;
+  isFaceUp: boolean;
+}
+
+const Memorize = () => {
+  const cards: ICard[] = [];
+  const [score, setScore] = useState(0);
+  //const [isFaceUp, setIsFaceUp] = useState(false);
+  const [selectedCars, setSelectedCars] = useState<number[]>([]);
+
+  const emoji = ["💘", "🚂", "🏹", "🤞"];
+  const [card, setCard] = useState<ICard[]>(() => {
+    for (let index = 0; index < emoji.length; index++) {
+      let content = emoji[index];
+      cards.push({ id: index * 2, content, isFaceUp: false });
+      cards.push({ id: index * 2 + 1, content, isFaceUp: false });
+    }
+
+    return cards;
+  });
+
+  useEffect(() => {}, []);
+
+  function handleCellClick(id: number) {
+    const alreadySelected = selectedCars.findIndex((cardId) => cardId === id);
+
+    if (alreadySelected !== -1) {
+      return;
+    } else {
+      setSelectedCars([...selectedCars, id]);
+      setCard(
+        card.map((item) =>
+          item.id === id ? { ...item, isFaceUp: true } : item
+        )
+      );
+    }
+
+    //console.log(selectedCars.length);
+
+    selectedCars.map((item) => console.log(item));
+
+    /*if (alreadySelected >= 0) {
+      //setSelectedCars([id]);
+      //return;
+    } else {
+      setSelectedCars([...selectedCars, id]);
+      setCard(
+        card.map((item) =>
+          item.id === id ? { ...item, isFaceUp: true } : item
+        )
+      );
+    }
+
+    if (selectedCars.length === 1) {
+      console.log(selectedCars[0]);
+      console.log(selectedCars[1]);
+
+      const card1 = card.filter((item) => item.id === selectedCars[0]);
+
+      const card2 = card.filter((item) => item.id === selectedCars[1]);
+      //console.log([card1, card2]);
+    }*/
+  }
+
   return (
     <main>
       <h1 className="title-base title">Memorize</h1>
 
       <div className="board">
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
-        <div className="cell">❤</div>
+        {card.map((item, index) => (
+          <div
+            key={index}
+            className="cell"
+            onClick={() => handleCellClick(item.id)}
+          >
+            {item.isFaceUp && item.content}
+          </div>
+        ))}
       </div>
 
-      <h1 className="title-base footer">Score: 0</h1>
+      <h1 className="title-base footer">Score: {score}</h1>
     </main>
   );
-}
+};
 
 export default Memorize;
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Memorize from "./Memorize";
 
-ReactDOM.render(<Memorize />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Memorize />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
