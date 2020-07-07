0dd8cf15a1d98daccd7d154bd1b4ad2911ffe4f0
Valter-de-Brito <valter.dbrito@gmail.com>
2020-07-07 13:36:19 +0100

    Programming FaceUP / FaceDown and isMatched

--- src/Memorize.tsx
+++ src/Memorize.tsx
@@ -6,66 +6,75 @@ interface ICard {
   id: number;
   content: string;
   isFaceUp: boolean;
+  isMatched: boolean;
 }
 
 const Memorize = () => {
   const cards: ICard[] = [];
   const [score, setScore] = useState(0);
-  //const [isFaceUp, setIsFaceUp] = useState(false);
   const [selectedCars, setSelectedCars] = useState<number[]>([]);
 
   const emoji = ["💘", "🚂", "🏹", "🤞"];
   const [card, setCard] = useState<ICard[]>(() => {
     for (let index = 0; index < emoji.length; index++) {
       let content = emoji[index];
-      cards.push({ id: index * 2, content, isFaceUp: false });
-      cards.push({ id: index * 2 + 1, content, isFaceUp: false });
+      cards.push({ id: index * 2, content, isFaceUp: false, isMatched: false });
+      cards.push({
+        id: index * 2 + 1,
+        content,
+        isFaceUp: false,
+        isMatched: false,
+      });
     }
 
     return cards;
   });
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(selectedCars);
+
+    if (selectedCars.length === 2) {
+      const isMatched =
+        card[selectedCars[0]].content === card[selectedCars[1]].content;
+
+      if (isMatched) {
+        card.forEach((element) => {
+          if (element.isFaceUp === true) {
+            element.isMatched = true;
+          }
+        });
+
+        setScore(score + 2);
+      } else {
+        card.forEach((element) => {
+          if (element.isFaceUp === true && element.isMatched === false) {
+            element.isFaceUp = false;
+          }
+        });
+
+        setScore(score - 1);
+      }
+
+      setSelectedCars([]);
+    }
+  }, [card, score, selectedCars]);
 
   function handleCellClick(id: number) {
     const alreadySelected = selectedCars.findIndex((cardId) => cardId === id);
+    const [isMatched] = card.filter((itemCard) => itemCard.id === id);
 
-    if (alreadySelected !== -1) {
-      return;
-    } else {
-      setSelectedCars([...selectedCars, id]);
-      setCard(
-        card.map((item) =>
-          item.id === id ? { ...item, isFaceUp: true } : item
-        )
-      );
+    if (isMatched.isMatched !== true) {
+      if (alreadySelected !== -1) {
+        return;
+      } else {
+        setSelectedCars([...selectedCars, id]);
+        setCard(
+          card.map((item) =>
+            item.id === id ? { ...item, isFaceUp: true } : item
+          )
+        );
+      }
     }
-
-    //console.log(selectedCars.length);
-
-    selectedCars.map((item) => console.log(item));
-
-    /*if (alreadySelected >= 0) {
-      //setSelectedCars([id]);
-      //return;
-    } else {
-      setSelectedCars([...selectedCars, id]);
-      setCard(
-        card.map((item) =>
-          item.id === id ? { ...item, isFaceUp: true } : item
-        )
-      );
-    }
-
-    if (selectedCars.length === 1) {
-      console.log(selectedCars[0]);
-      console.log(selectedCars[1]);
-
-      const card1 = card.filter((item) => item.id === selectedCars[0]);
-
-      const card2 = card.filter((item) => item.id === selectedCars[1]);
-      //console.log([card1, card2]);
-    }*/
   }
 
   return (
@@ -76,7 +85,7 @@ const Memorize = () => {
         {card.map((item, index) => (
           <div
             key={index}
-            className="cell"
+            className={item.isMatched ? "cell matched" : "cell"}
             onClick={() => handleCellClick(item.id)}
           >
             {item.isFaceUp && item.content}
